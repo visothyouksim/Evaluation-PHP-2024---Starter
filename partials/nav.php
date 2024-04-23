825ff9e260fd53984c63baee8bf30b7800784da8
Visoth YOUK-SIM <115548934+visothyouksim@users.noreply.github.com>
2024-04-23 12:15:58 +0200

    Update nav.php

--- partials/nav.php
+++ partials/nav.php
@@ -5,7 +5,7 @@
 
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-        <a class="navbar-brand" href="accueil.html">Forum News - Vivez l'info !</a>
+        <a class="navbar-brand" href="index.php">Forum News - Vivez l'info !</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -13,7 +13,7 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav mx-auto text-center">
                 <li class="nav-item">
-                    <a class="nav-link" href="accueil.html">Accueil</a>
+                    <a class="nav-link" href="index.php">Accueil</a>
                 </li>
                 <?php foreach ($forums as $forum) { ?>
                     <li class="nav-item">
